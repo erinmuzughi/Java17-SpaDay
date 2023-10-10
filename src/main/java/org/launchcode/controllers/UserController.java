2524f87a1c6fd591c0a11a12bfd9736a47d366d3
erinmuzughi <erinmuzughi@gmail.com>
2023-10-09 20:45:28 -0500

    finished studio plus bonus

--- src/main/java/org/launchcode/controllers/UserController.java
+++ src/main/java/org/launchcode/controllers/UserController.java
@@ -1,34 +1,38 @@
 package org.launchcode.controllers;
 
+import jakarta.validation.Valid;
 import org.launchcode.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.validation.Errors;
 
 @Controller
 @RequestMapping("user")
 public class UserController {
 
     @GetMapping("/add")
-    public String displayAddUserForm() {
+    public String displayAddUserForm(Model model, @ModelAttribute User user) {
+        model.addAttribute("user", new User());
         return "user/add";
     }
 
     @PostMapping
-    public String processAddUserForm(Model model, @ModelAttribute User user, String verify) {
-        model.addAttribute("user", user);
-        model.addAttribute("verify", verify);
-        model.addAttribute("username", user.getUsername());
-        model.addAttribute("email", user.getEmail());
-        if (user.getPassword().equals(verify)) {
-            return "user/index";
-        }
-        else {
-            model.addAttribute("error", "Passwords do not match");
+    public String processAddUserForm(@ModelAttribute @Valid User user, Errors errors, Model model) {
+        if (errors.hasErrors()) {
             return "user/add";
         }
+        else {
+            return "user/index";
+        }
+//            if (user.getPassword().equals(user.getVerify())) {
+//                return "user/index";
+//            }
+//            else {
+//                model.addAttribute("error", "Passwords do not match");
+//                return "user/add";
+//            }
 
-    }
-
-
+        }
+//    }
 }
--- src/main/java/org/launchcode/models/User.java
+++ src/main/java/org/launchcode/models/User.java
@@ -1,21 +1,42 @@
 package org.launchcode.models;
 
+import jakarta.validation.constraints.*;
+
 public class User {
-    private String username;
-    private String email;
+
+    @NotBlank(message = "Username is required.")
+    @Size(min = 5, max = 15, message = "Username must be between 5 and 15 characters.")
+    String username;
+    @Email(message = "Email must be a valid email address.") //this doesn't work properly to validate
+    String email;
+    @NotBlank(message = "Password is required.")
+    @Size(min = 6, message = "Password must be at least 6 characters long.")
     private String password;
+
+    @NotNull(message = "Passwords do not match.")
+    @Size(min = 6, message = "Verify Password must be at least 6 characters long.")
+    private String verify;
 
     public User() {
 
     }
 
-    public User(String username, String email, String password) {
+    public User(String username, String email, String password, String verify) {
         this();
         this.username = username;
         this.email = email;
         this.password = password;
+        this.verify = verify;
     }
 
+    // Add a new method, private void checkPassword, that compares password and verifyPassword.
+    // If neither is null and they don’t match, then set verifyPassword = null.
+
+    private void checkPassword() {
+        if(password!=null && verify!=null && !password.equals(verify)){
+            verify = null;
+        }
+    }
     public String getUsername() {
         return username;
     }
@@ -38,6 +59,16 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+        checkPassword();
+    }
+
+    public String getVerify() {
+        return verify;
+    }
+
+    public void setVerify(String verify) {
+        this.verify = verify;
+        checkPassword();
     }
 }
 
